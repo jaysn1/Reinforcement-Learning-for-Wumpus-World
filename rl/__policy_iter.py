e23fa55c3c812fc1e9dfa3d63537b8bac55436ab
piyush2896 <piyushmalhotra28@gmail.com>
2020-04-25 17:12:56 -0700

    Add policy iteration

--- rl/__policy_iter.py
+++ rl/__policy_iter.py
@@ -1,12 +1,30 @@
+import numpy as np
+
 from .__reinforcement_learner import ReinforcementLearnerBase
 
 class PolicyIter(ReinforcementLearnerBase):
-    def __init__(self, env):
+    def __init__(self, env, gamma):
         super().__init__(env)
-        self.__pi_star
+        self.gamma = gamma
+        self.__is_ran = False
+        self.__pi_star = np.ones(
+            (self.env.n_states, self.env.n_actions)) / self.env.n_actions
 
-    def run(self):
-        pass
+    def run(self, H):
+        for i in range(H):
+            is_policy_good = False
+            V = self.__eval_policy()
+            for state in range(self.env.n_states):
+                action = np.argmax(self.__pi_star[state])
+                values = self.__1step_lookahead(state, V)
+                best_action = np.argmax(values)
+                if action != best_action:
+                    is_policy_good = True
+                    self.__pi_star[state] = np.eye(self.env.n_actions)[best_action]
+            if is_policy_good:
+                break
+        self.__is_ran = True
 
     def next_action(self, state):
-        pass
+        assert self.__is_ran, "Can't get next action without fitting the model. First call PolicyIter().run()"
+        return np.argmax(self.__pi_vals[state, :])
