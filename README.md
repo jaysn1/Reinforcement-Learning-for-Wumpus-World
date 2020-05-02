b66feb36e60ebc4c88715af2af1a26cd96457527
piyush2896 <piyushmalhotra28@gmail.com>
2020-05-01 19:39:59 -0700

    Command line arg handler and Readme update

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# CSE-571-Spring2020_RL4Wumpus
+# Reinforcement Learning Agent in Wumpus World
+In this project, multiple different reinforcement learning algorithms including policy iteration and value iteration were run on a noisy Wumpus world and compared to a hybrid logic agent.
+
+## Dependencies
+1. Python 3
+2. NumPy
+
+
+## How to run
+To see commands associated with the file:
+
+`python main.py --help`
+
+```
+usage: main.py [-h] [--algo ALGO] [--gamma GAMMA] [--noise NOISE] [--h H]
+               [--width WIDTH] [--height HEIGHT] [--start START [START ...]]
+               [--goal GOAL [GOAL ...]] [--wumpus WUMPUS [WUMPUS ...]]
+               [--pit PIT [PIT ...]]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --algo ALGO           Algorithm to use - Value Iteration or Policy Iteration
+                        [p|V]
+  --gamma GAMMA         Gamma Value, Default 0.9
+  --noise NOISE         Noise Value, Default 0.2
+  --h H                 Horizon Value, Default 20
+  --width WIDTH         Width of the world, default 4
+  --height HEIGHT       height of the world, default 4
+  --start START [START ...]
+                        The start position of agent.
+  --goal GOAL [GOAL ...]
+                        Location of gold
+  --wumpus WUMPUS [WUMPUS ...]
+                        Wumpus location.
+  --pit PIT [PIT ...]   Pit location to add multiple use it multiple times.
+```
+
+To run a world:
+
+`python main.py --algo P --width 4 --height 4 --start 3 0 --goal 1 2 --pit 1 1 --pit 3 2 --wumpus 1 0`
+
+The environment associated with this:
+
+```
+      |  0  |  1  |  2  |  3  |
+-------------------------------
+|    0|     |     |     |     |
+|    1|  W  |  P  |  G  |     |
+|    2|     |     |     |     |
+|    3|  S  |     |  P  |     |
+```
+
+Here,
+
+```
+Things and Abbreviations looking at Env:
+S - Start Point
+G - GOLD  P - PIT
+W - WUMPUSX - STOP
+```
--- main.py
+++ main.py
@@ -14,17 +14,39 @@ def run(algo_cls, H, gamma, wumpus_config):
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--algo', type=str, help="Algorithm to use - Value Iteration or Policy Iteration [p|V]",
+                        default='P')
+    parser.add_argument('--gamma',  type=float, help="Gamma Value, Default 0.9",
+                        default=0.9)
+    parser.add_argument('--noise',  type=float, help="Noise Value, Default 0.2",
+                        default=0.2)
+    parser.add_argument('--h',  type=int, help="Horizon Value, Default 20",
+                        default=20)
+    parser.add_argument('--width', type=int, help='Width of the world, default 4', default=4)
+    parser.add_argument('--height', type=int, help='height of the world, default 4', default=4)
+    parser.add_argument('--start', type=int, nargs='+', help="The start position of agent.")
+    parser.add_argument('--goal', type=int, nargs='+', help="Location of gold")
+    parser.add_argument('--wumpus', type=int, nargs='+', help="Wumpus location.")
+    parser.add_argument('--pit', type=int, nargs='+', action='append',
+                        help="Pit location to add multiple use it multiple times.")
+
+    args = parser.parse_args()
     wumpus_config = {
-        'width': 4,
-        'height': 4,
-        'start': [3, 0],
-        'wumpus_loc': [1, 0],
-        'pit_locs': [[1, 1], [3, 2]],
-        'goal':[1, 2],
-        'noise': 0.2
+        'width': args.width,
+        'height': args.height,
+        'start': args.start,
+        'wumpus_loc': args.wumpus,
+        'pit_locs': args.pit,
+        'goal':args.goal,
+        'noise': args.noise
     }
 
-    iterator, env, v_or_pi = run(rl.PolicyIter, 20, 0.7, wumpus_config)
+    if args.algo.upper() == 'P':
+        algo = rl.PolicyIter
+    else:
+        algo = rl.ValueIter
+    iterator, env, v_or_pi = run(algo, args.h, args.gamma, wumpus_config)
     import numpy as np
     pi = []
     for i in range(env.n_states):
